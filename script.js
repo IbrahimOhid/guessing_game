ca61b6309f89d9c490b55e9e886934d8d263193f
unknown <ibrahimohid77@gmail.com>
2024-09-15 13:21:56 +0600

    desing part added

--- script.js
+++ script.js
@@ -9,6 +9,10 @@
 
     let correctNum = 0;
     let wrongNum = 0;
+    guessBtnEle.setAttribute('disabled', 'disabled');
+
+    
+
 
     function guessConditionState() {
         const randomNum = Math.floor(Math.random() * 10);
@@ -16,16 +20,13 @@
         const newP = document.createElement('p');
         newDivEle.appendChild(newP);
 
-        
-
         if (randomNum === inputValue) {
             correctNum++;
             correctNumEle.textContent = correctNum;
             newP.style.cssText = 'border: 2px solid green; padding: 5px;'
             newP.textContent = `Wow great!!`;
-
-
-        } else {
+        }
+        else {
             wrongNum++;
             wrongNumEle.textContent = wrongNum;
             newP.style.cssText = 'border: 2px solid red; padding: 5px;';
@@ -35,13 +36,19 @@
         inputNumEle.value = '';
     }
 
-
-
+    function mainGuessState(){
+        const inputValue = +inputNumEle.value;
+        console.log(inputValue)
+        if(inputValue <= 9){
+            guessBtnEle.removeAttribute('disabled');
+        }
+    }
 
     guessBtnEle.addEventListener('click', (e) => {
         e.preventDefault();
+        mainGuessState();
         guessConditionState();
-
+        
 
     })
 })();
