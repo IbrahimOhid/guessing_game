a5d8d93dceca9766bcf47b819d70de2c2ae26921
unknown <ibrahimohid77@gmail.com>
2024-09-15 12:36:44 +0600

    guessing game code start

---
+++ script.js
@@ -0,0 +1,77 @@
+// Selector
+(function () {
+    const inputNumEle = document.querySelector('#inputNum');
+    const guessBtnEle = document.querySelector('#guessBtn');
+    const newDivEle = document.querySelector('#newDiv');
+    const correctNumEle = document.querySelector('#correctNum');
+    const wrongNumEle = document.querySelector('#wrongNum');
+    guessBtnEle.style.marginBottom = '20px';
+
+    let correctNum = 0;
+    let wrongNum = 0;
+
+    function guessConditionState() {
+        const randomNum = Math.floor(Math.random() * 10);
+        const inputValue = +inputNumEle.value;
+        const newP = document.createElement('p');
+        newDivEle.appendChild(newP);
+
+        
+
+        if (randomNum === inputValue) {
+            correctNum++;
+            correctNumEle.textContent = correctNum;
+            newP.style.cssText = 'border: 2px solid green; padding: 5px;'
+            newP.textContent = `Wow great!!`;
+
+
+        } else {
+            wrongNum++;
+            wrongNumEle.textContent = wrongNum;
+            newP.style.cssText = 'border: 2px solid red; padding: 5px;';
+            newP.textContent = `Sorry!! Wrong Digit. It Was  ${randomNum}`;
+
+        }
+        inputNumEle.value = '';
+    }
+
+
+
+
+    guessBtnEle.addEventListener('click', (e) => {
+        e.preventDefault();
+        guessConditionState();
+
+
+    })
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
